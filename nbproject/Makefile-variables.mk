c88a2e21ebbb66517a570291438530c9719c5ae3
Jo <Jo@DESKTOP-19JQCVF>
2016-02-19 09:51:23 +0100

    aaa

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
+CND_ARTIFACT_NAME_Debug=projet_-_netbeans
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/projet_-_netbeans
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
+CND_PACKAGE_NAME_Debug=projet-netbeans.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/projet-netbeans.tar
+# Release configuration
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
+CND_ARTIFACT_NAME_Release=projet_-_netbeans
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/projet_-_netbeans
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
+CND_PACKAGE_NAME_Release=projet-netbeans.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/projet-netbeans.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
